8f3dddf39bed18a8f13863e34fe43e8f59d7539e
Zhiming <xiaoxiaomingbao@gmail.com>
2024-07-29 10:37:00 +0800

    修改创建发包的程序

--- src/main/java/org/example/snsproject/mapper/ProjectCreationMapper.java
+++ src/main/java/org/example/snsproject/mapper/ProjectCreationMapper.java
@@ -10,6 +10,6 @@ public interface ProjectCreationMapper {
 
     @Insert("insert into project (uid, category, title, info, deadline, price_lower, price_upper, tel, receiver, create_time) " +
             "values (#{uid}, #{category}, #{title}, #{info}, #{deadline}, #{priceLower}, #{priceUpper}, #{tel}, #{receiver}, #{createTime})")
-    @Options(useGeneratedKeys = true, keyProperty = "id") // 自动填充主键ID到实体类中
+    @Options(useGeneratedKeys = true, keyProperty = "pid") // 自动填充主键ID到实体类中
     int createProject(Project project);
 }
